ea5265315e7c053cd87cd08f6253adbe706638ea
Brian Karabinchak <karabinchak.brian@gmail.com>
2017-12-14 08:34:36 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//Node only has support for common JS modules
+const express = require("express");
+// import express from 'express';
+// this is ES2015 modules, Node doesn't currently have support
+// for this
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send({ hi: "there" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
